5f535611754efece65ce7d00e40309b443b5c1c4
Giovanni <dibellogio@gmail.com>
2024-12-18 13:49:37 +0100

    add logic

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// creo un ciclo for per i numeri da 1 a 100
+    // SE è un multiplo di 3
+    // stampo Fizz
+    // SE è un multiplo di 5 
+    // stampo Buzz
+    // Se è un multiplo sia di 3 che di 5
+    // stampo FizzBuzz
